90d2cc8e36e9b08b9bcb4d2b0d068c162b8f6c50
Dov Rosenberg <dov@rosenbergmail.com>
2025-03-01 09:48:04 -0500

    tests

--- test/index.ts
+++ test/index.ts
@@ -2,6 +2,8 @@
 // Quench Unit Testing              //
 // -------------------------------- //
 
+import { ModuleSettingKeys, ModuleSettings } from '@/settings/ModuleSettings';
+import { log } from '@/utils/log';
 import { registerUtilTests } from '@test/utils';
 import { registerWeatherTests } from '@test/weather';
 
@@ -10,3 +12,17 @@ Hooks.on("quenchReady", () => {
   registerUtilTests();
   registerWeatherTests();
 });
+
+const settings = {};
+
+export const backupSettings = () => {
+  for (const k of Object.values(ModuleSettingKeys)) {
+    settings[k] = ModuleSettings.get(k as ModuleSettingKeys);
+  }
+}
+
+export const restoreSettings = async () => {
+  for (const k of Object.values(ModuleSettingKeys)) {
+    await ModuleSettings.set(k as ModuleSettingKeys, settings[k]);
+  }
+}
--- test/weather/index.ts
+++ test/weather/index.ts
@@ -1,7 +1,9 @@
 import { registerClimateDataTests } from './climateData.test';
 import { registerEffectsMapTests } from './effectsMap.test';
+import { registerForecastTests } from './Forecast.test';
 
 export const registerWeatherTests = () => {
   registerClimateDataTests();
   registerEffectsMapTests();
+  registerForecastTests();
 }
